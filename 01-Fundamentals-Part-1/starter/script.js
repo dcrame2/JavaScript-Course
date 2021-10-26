1caaa4b6e1d12302079d1f5e62f78505a45b95e2
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-10-26 05:55:51 -0500

    practicing simple js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -20,8 +20,62 @@ console.log(year);
 console.log(typeof year);
 */
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthYear = 1991;
-birthYear = 1990;
+// const birthYear = 1991;
+// birthYear = 1990;
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Johnny';
+
+console.log(firstName + " " + lastName);
+
+
+//Assignment operators
+let x = 10 + 5;
+x += 10 //x = x + 10 = 25
+x *= 4; // x = x *4
+console.log(x);
+
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const avgAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, avgAge);
+*/
+
+
+const markHeight = 1.69;
+const markWeight = 78;
+const johnHeight = 1.95;
+const johnWeight = 92;
+
+const markBMI = markWeight / (markHeight ** 2);
+const johnBMI = johnWeight / (johnHeight ** 2);
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
