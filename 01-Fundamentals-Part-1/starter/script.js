d71d466d77e6e6a71df0f5161822f3455c8599b2
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-10-25 20:43:46 -0500

    finishing assignment 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,27 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+/*
+true;  //automatically a boolean
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+//Dynamic Typing
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;
