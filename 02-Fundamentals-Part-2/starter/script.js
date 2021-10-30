1a1aab75aa00e0a7816b3e1d35c1d698cf1539d5
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-10-30 08:27:25 -0500

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,36 @@
 'use strict';
 
-let hasDriversLicense = false;
+/*let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive!");
+if (hasDriversLicense) console.log("I can drive!");*/
 
 // const interface = 'Audio';
 // const private = 534;
+
+
+function logger() {
+    console.log("my name is jonas");
+}
+
+
+// calling / running / invoking the function
+logger();
+
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+
