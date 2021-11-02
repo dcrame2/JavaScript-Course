18a5b3db99d39a317ad596d00c3164c9f1f74d9e
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-11-02 06:13:38 -0500

    coding challenge #3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -71,11 +71,84 @@ const yearUntiltRetirement = (birthYear, firstName) => {
 }
 
 console.log(yearUntiltRetirement(1991, "Dylan"));
+*/
+
+
+
+
+//Using Multiple functions
 /*
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+*/
 
 
 
+//Using an if/else in your function
+/*
+const calcAge = function (birthYear) {
+    return 2021 - birthYear;
+}
+
+
+const yearUntiltRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        return retirement;
+    } else {
+        return -1;
+    }
+
+
+    // return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearUntiltRetirement(1996, 'Dylan'));
+console.log(yearUntiltRetirement(1917, 'Mike'));
+*/
+
+
+//Coding Challenge #3
+
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+
+//Test 1
+// const avgDolphins = calcAverage(44, 23, 71);
+// const avgKoalas = calcAverage(65, 54, 49);
+
+// Test 2 
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
 
 
 
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= avgKoalas * 2) {
+        return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`;
+    } else if (avgKoalas >= avgDolphins * 2) {
+        return `Koalas win (${avgKoalas} vs. ${avgDolphins})`;
+    } else {
+        return "Nobody wins!"
+    }
+}
+
+const winner = checkWinner(scoreDolphins, scoreKoalas);
+console.log(winner);
 
