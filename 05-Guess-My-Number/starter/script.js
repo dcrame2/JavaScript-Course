f9caabfc2a1958b90a0f965ee6f47e5c27300816
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-11-16 20:03:50 -0600

    guess game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -9,3 +9,24 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+//secret number needs to be outside the listener
+const number = Math.floor(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+console.log(number);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (number === guess) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > number) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < number) {
+    document.querySelector('.message').textContent = 'Too low!';
+  }
+});
