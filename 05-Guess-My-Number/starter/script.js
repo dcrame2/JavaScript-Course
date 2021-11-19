78ed6c20f33989964cfde41dcfc81c2c14abc52a
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-11-19 05:33:05 -0600

    refactoring #2

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -16,16 +16,21 @@ let number = Math.floor(Math.random() * 20) + 1;
 let highscore = 0;
 let score = 20;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
-
+    // document.querySelector('.message').textContent = 'No number!';
+    displayMessage('No number!');
     // When player wins
   } else if (number === guess) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    // document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number!');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = number;
 
@@ -39,12 +44,14 @@ document.querySelector('.check').addEventListener('click', function () {
     // When guess is wrong
   } else if (guess !== number) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > number ? 'Too high!' : 'Too low!';
+      //   document.querySelector('.message').textContent =
+      //     guess > number ? 'Too high!' : 'Too low!';
+      displayMessage(guess > number ? 'Too high!' : 'Too low!');
       score--; //score = score - 1;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      //   document.querySelector('.message').textContent = 'You lost the game!';
+      displayMessage('You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -78,7 +85,8 @@ document.querySelector('.again').addEventListener('click', function () {
   number = Math.floor(Math.random() * 20) + 1;
 
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  //   document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
