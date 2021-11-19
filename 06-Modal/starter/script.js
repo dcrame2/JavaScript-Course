3695450d18ff3bf8fec48f238474b8f9b0c79be3
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-11-19 05:53:11 -0600

    modal prep

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+console.log(btnShowModal);
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', function () {
+    console.log('button clicked!');
+  });
+}
