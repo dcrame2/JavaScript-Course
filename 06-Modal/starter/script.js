7cbbf097a5022239604e7c2facbdc6057eb9acb6
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-11-21 11:39:35 -0600

    modal update #2

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,10 +5,22 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
-console.log(btnShowModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//opening the modal
 for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener('click', function () {
-    console.log('button clicked!');
-  });
+  btnShowModal[i].addEventListener('click', openModal);
 }
+
+// closing the modal via X button
+btnCloseModal.addEventListener('click', closeModal);
+// closing the modal via clicking on the overlay
+overlay.addEventListener('click', closeModal);
