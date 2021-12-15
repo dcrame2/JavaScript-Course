75673c3e0ec7598ee6e0cc43526a620f11cbe354
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-12-15 05:42:35 -0600

    scope #1

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  //creates new scope
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Dylan';
+calcAge(1996);
