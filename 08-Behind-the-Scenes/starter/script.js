2a9bf444013f8560f8245811eb2b24fbef7e0539
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-12-16 20:03:11 -0600

    Hoisting #1

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -34,3 +34,43 @@ function calcAge(birthYear) {
 
 const firstName = 'Dylan';
 calcAge(1991);
+*/
+
+// Hoisting: with variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+//Hositing: with functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+function addDecl(a, b) {
+  return a + b;
+}
+
+var addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Hositing: Exampple
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
