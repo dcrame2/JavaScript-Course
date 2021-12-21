bdbe94788b37a4ee556207ad917de76f52790f56
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-12-20 18:23:29 -0600

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -36,6 +36,7 @@ const firstName = 'Dylan';
 calcAge(1991);
 */
 
+/*
 // Hoisting: with variables
 console.log(me);
 // console.log(job);
@@ -74,3 +75,38 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+// this keyword
+
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  //   console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
