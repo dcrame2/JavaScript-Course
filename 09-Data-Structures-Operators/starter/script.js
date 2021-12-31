64cc04a8469f7eebb7b8184bd195d7197d956799
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2021-12-30 18:22:58 -0600

    && ||

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,6 +53,39 @@ const restaurant = {
     },
   },
 };
+
+// && and ||
+
+console.log('--- OR ---');
+// Use ANY data type, return ANY data type, short circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1); // turnary operator
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('--- AND ---');
+
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'Jonas');
+
+// Practical Example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.order('mushrooms', 'spinach');
+
 /*
 // 1) Deconstructoring
 
