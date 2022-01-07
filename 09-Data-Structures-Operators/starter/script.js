aafaf0fd696dd61fb94c935b732d7a0d085dfad4
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-01-07 06:05:05 -0600

    Looping objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -58,6 +58,29 @@ const restaurant = {
   },
 };
 
+// Property NAMES
+const prop = Object.keys(openingHours);
+console.log(prop);
+
+let openStr = `We are open on ${prop.length} days: `;
+
+for (const day of prop) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+//Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire Object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} open at ${open} and close at ${close}`);
+}
+/*
 // WITHOUT Optional Chaining
 if (restaurant.openingHours && restaurant.openingHours.mon)
   console.log(restaurant.openingHours.mon.open);
@@ -72,6 +95,15 @@ for (const day of days) {
   console.log(`On day ${day}, we open at ${open}`);
 }
 
+//Methods
+console.log(restaurant.order?.(0, 1 ?? 'Method does not exist!'));
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist!');
+
+// Arrays
+const user = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+
+console.log(user[0]?.name ?? 'User array empty');
+*/
 // For-of operator
 /*const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
