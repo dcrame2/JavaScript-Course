84b40b2ac94ed374c61d16fd2a707140f82aacba
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-01-18 05:38:37 -0600

    Maps: iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -58,7 +58,36 @@ const restaurant = {
   },
 };
 
-// SETS
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close'))); // don't overuse this
+
+console.log(rest.has('categories'));
+rest.delete(2);
+
+const arr = [1, 2];
+rest.set(arr, 'test');
+rest.set(document.querySelector('h1'), 'heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+/*// SETS
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -88,6 +117,8 @@ const staffUnique = [...new Set(staff)]; //spread operator works the same as an 
 console.log(staffUnique);
 console.log(new Set(staff).size);
 
+
+*/
 /*
 // Property NAMES - 
 const prop = Object.keys(openingHours);
