f1172fa2f167d7d5ebc84cbb2b7052c3716bc6c6
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-01-25 05:58:19 -0600

    CODING CHALLENGE #3
    Challenge using Maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -636,16 +636,40 @@ for (const [team, odd] of Object.entries(game.odds)) {
 // ⚽
 // GOAL
 // GOOD LUCK 😀
-// const gameEvents = new Map([
-// [17, '⚽ GOAL'],
-// [36, '🔁 Substitution'],
-// [47, '⚽ GOAL'],
-// [61, '🔁 Substitution'],
-// [64, '🔶 Yellow card'],
-// [69, '🔴 Red card'],
-// [70, '🔁 Substitution'],
-// [72, '🔁 Substitution'],
-// [76, '⚽ GOAL'],
-// [80, '⚽ GOAL'],
-// [92, '🔶 Yellow card'],
-// ]);
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+//IN CORRECT
+// for (const [min, events] of gameEvents) {
+//   console.log(events);
+// }
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+
+//3.
+const time = [...gameEvents.keys()].pop();
+console.log(`An event happened, on
+ average, every ${time / gameEvents.size} minutes`);
+// Bonus
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
