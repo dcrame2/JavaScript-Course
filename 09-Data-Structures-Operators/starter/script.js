73e31f6360c5bdc6ce468cf4dd3109df5cd0a843
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-03 19:34:31 -0600

    last string examples

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekdays[3]]: {
@@ -58,6 +54,29 @@ const restaurant = {
   },
 };
 
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Delayed Departure from FAO to TXL (11h25)
+//          Arrivial from BRU to FAO (11h45)
+//   Delayed Arrival from HEL to FAO (12h05)
+//         Departure from FAO to LIS (12h30)
+
+console.log(flights.split('+'));
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${from.slice(0, 3).toUpperCase()} to ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(':', 'h')})`.padStart(50);
+  console.log(output);
+}
+
+/*
 /////////////////////////////////////
 // Coding Challenge #4
 // Write a program that receives a list of variable names written in underscore_case
@@ -105,7 +124,7 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
-
+*/
 /*
 ////////////////////////////////////////
 // Working With Strings - Part 3
