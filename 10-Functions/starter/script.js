976dc49b1c0843ab0e38e09e291b29328e090a86
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-04 22:47:06 -0600

    functions 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNumber, numPassengers = 1, price = 199) {
+  const booking = {
+    flightNumber,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', undefined, 800);
