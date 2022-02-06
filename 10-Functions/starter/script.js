f4d0ea0d5d810bc8dcf1544f96edea853925fb26
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-06 11:45:26 -0600

    functions 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+// Function Example
 const bookings = [];
 
 const createBooking = function (flightNumber, numPassengers = 1, price = 199) {
@@ -15,3 +17,38 @@ const createBooking = function (flightNumber, numPassengers = 1, price = 199) {
 createBooking('LH123');
 createBooking('LH123', 2, 800);
 createBooking('LH123', undefined, 800);
+*/
+
+// How passing arugments works: references vs primitives - applied to functions
+// being aware of this issue
+const flight = 'LH234';
+const jonas = {
+  name: 'Dylan Cramer',
+  passport: 234542365,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 234542365) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightName = flight;
+// const passanger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
