3120fb3171fef06c4454899d89f7ab87eebc896e
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-07 05:35:48 -0600

    functions 3

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -19,6 +19,7 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', undefined, 800);
 */
 
+/*
 // How passing arugments works: references vs primitives - applied to functions
 // being aware of this issue
 const flight = 'LH234';
@@ -52,3 +53,32 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callback functions all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
