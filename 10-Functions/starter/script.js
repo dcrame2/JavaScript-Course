ea44c1f021cdfac44920105d4d812f6038535c0c
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-09 05:47:11 -0600

    coding challenge 1 finished

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -242,12 +242,22 @@ const poll = {
       answer < this.options.length &&
       this.answers[answer]++;
 
-    console.log(this.answers);
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (typeof type === 'array') {
+      console.log(this.answers);
+    } else if (typeof type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')} `);
+    }
   },
 };
 
 document
   .querySelector('.poll')
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 
 // poll.registerNewAnswer();
