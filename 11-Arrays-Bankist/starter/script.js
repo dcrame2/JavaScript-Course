8acd085d44c08c06699dfd2d9c1e48cb0e400fe0
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-16 05:52:26 -0600

    forEach()

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -108,6 +108,7 @@ console.log([...arr, ...arr2]);
 console.log(letters.join(' - '));
 */
 
+/*
 // AT
 const arr = [23, 11, 64];
 console.log(arr[0]);
@@ -119,3 +120,26 @@ console.log(arr.slice(-1)[0]);
 console.log(arr.at(-1));
 
 console.log('Dylan'.at(0));
+*/
+
+console.log('---- FOROF ----');
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// You can't break out of a foreach loop - it will always loop over the whole array.
