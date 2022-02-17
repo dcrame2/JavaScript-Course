901aabee2f4fa7f8f5059b5126ec989fb2248ca6
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-17 07:27:52 -0600

    foreach for sets and maps

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -122,6 +122,7 @@ console.log(arr.at(-1));
 console.log('Dylan'.at(0));
 */
 
+/*
 console.log('---- FOROF ----');
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //for (const movement of movements) {
@@ -143,3 +144,23 @@ movements.forEach(function (mov, i, arr) {
 });
 
 // You can't break out of a foreach loop - it will always loop over the whole array.
+*/
+
+// FOR EACH FOR MAPS
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// FOR EACH FOR SETS
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+// Sets dont have keys - it makes no sense to have it for sets.
