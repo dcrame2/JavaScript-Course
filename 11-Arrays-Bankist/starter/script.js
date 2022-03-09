7cd2c014e9d967adccf8abe04b56c9cf632b30b3
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-03-09 05:28:02 -0600

    Coding Challenege #2
    Using Reduce, filter and map

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -305,6 +305,7 @@ const withdrawals = movements.filter(function (mov) {
 console.log(withdrawals);
 */
 
+/*
 // REDUCE METHOD
 console.log(movements);
 
@@ -328,5 +329,48 @@ const max = movements.reduce(function (acc, mov) {
     return mov;
   }
 }, movements[0]); // always go with first value of the array
-
 console.log(max);
+*/
+
+///////////////////////////
+// Coding Challenge #2
+// Let's go back to Julia and Kate's study about dogs. This time, they want to convert
+// dog ages to human ages and calculate the average age of the dogs in their study.
+
+// Your tasks:
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's
+// ages ('ages'), and does the following things in order:
+// 1. Calculate the dog age in human years using the following formula: if the dog is
+// <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
+// humanAge = 16 + dogAge * 4
+// 2. Exclude all dogs that are less than 18 human years old (which is the same as
+// keeping dogs that are at least 18 years old)
+// 3. Calculate the average human age of all adult dogs (you should already know
+// from other challenges how we calculate averages)
+// 4. Run the function for both test datasets
+
+// Test data:
+//   Data 1: [5, 2, 4, 1, 15, 8, 3]
+//   Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+const calcAverageHumanAge = function (dogArr) {
+  const humanAge = dogArr.map(function (dogAge) {
+    if (dogAge <= 2) {
+      return 2 * dogAge;
+    } else if (dogAge > 2) {
+      return 16 + dogAge * 4;
+    }
+  });
+  const adults = humanAge.filter(dogAge => dogAge > 18);
+  console.log(adults);
+
+  const average =
+    adults.reduce(function (acc, dogAge) {
+      return acc + dogAge;
+    }, 0) / adults.length;
+
+  return average;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
