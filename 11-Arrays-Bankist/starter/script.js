f2bedc16db5a06a5ceb78675eda9f533d843c1bf
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-03-09 04:50:29 -0600

    more reduce method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -319,3 +319,14 @@ console.log(balance);
 let balance2 = 0;
 for (const mov of movements) balance2 += mov;
 console.log(balance2);
+
+//Maximun value using Reduce method : acc will keep track of the max number
+const max = movements.reduce(function (acc, mov) {
+  if (acc > mov) {
+    return acc;
+  } else {
+    return mov;
+  }
+}, movements[0]); // always go with first value of the array
+
+console.log(max);
